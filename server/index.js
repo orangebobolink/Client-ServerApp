6b9efd4306face04f4183497b0b593191ce19a9b
Osten Power <osten_power@mail.ru>
2023-03-28 17:23:32 +0300

    add dotenv config

--- server/index.js
+++ server/index.js
@@ -1,61 +1,61 @@
-const express = require("express")
+const express = require("express");
 const Hunter = require("./controllers");
 const { getReqData } = require("./utils");
+require("dotenv").config();
 
-const app = express()
+const app = express();
 
 const PORT = process.env.PORT || 5000;
 
 function sendJSON(obj, res) {
-    const json = JSON.stringify(obj)
-    res.send(json)
+  const json = JSON.stringify(obj);
+  res.send(json);
 }
 
 function checkObj(obj, res) {
-    if(obj) {
-        sendJSON(obj, res)
-    }
-    else {
-        res.status(404).send();
-    }
+  if (obj) {
+    sendJSON(obj, res);
+  } else {
+    res.status(404).send();
+  }
 }
 
 app.listen(PORT, () => {
-    console.log(`server started on port: ${PORT}`);
+  console.log(`server started on port: ${PORT}`);
 });
 
-app.get('/api/hunters', async (req, res) =>{
-    const hunters = await new Hunter().getHunters();
+app.get("/api/hunters", async (req, res) => {
+  const hunters = await new Hunter().getHunters();
 
-    checkObj(hunters, res)
-})
+  checkObj(hunters, res);
+});
 
-app.get('/api/hunters/:id', async (req, res) =>{
-    const id = req.params.id;
-    const hunter = await new Hunter().getHunter(id);
-    
-    checkObj(hunter, res)
-})
+app.get("/api/hunters/:id", async (req, res) => {
+  const id = req.params.id;
+  const hunter = await new Hunter().getHunter(id);
 
-app.post("/api/hunters", async (req, res) =>{
-    let hunter_data = await getReqData(req);
-    const jsonParse = JSON.parse(hunter_data);
-    let hunter = await new Hunter().createHunter(jsonParse);
+  checkObj(hunter, res);
+});
 
-    checkObj(hunter, res)
-})
+app.post("/api/hunters", async (req, res) => {
+  let hunter_data = await getReqData(req);
+  const jsonParse = JSON.parse(hunter_data);
+  let hunter = await new Hunter().createHunter(jsonParse);
 
-app.put("/api/hunters/:id.:firstName", async (req, res) =>{
-    const id = req.params.id;
-    const firstName = req.params.firstName
-    let updated_hunter = await new Hunter().updateHunter(id, firstName);
+  checkObj(hunter, res);
+});
 
-    checkObj(updated_hunter, res)
-})
+app.put("/api/hunters/:id.:firstName", async (req, res) => {
+  const id = req.params.id;
+  const firstName = req.params.firstName;
+  let updated_hunter = await new Hunter().updateHunter(id, firstName);
 
-app.delete("/api/hunters/:id", async (req, res) =>{
-    const id = req.params.id;
-    let message = await new Hunter().deleteHunter(id);
+  checkObj(updated_hunter, res);
+});
 
-    checkObj(message, res)
-})
+app.delete("/api/hunters/:id", async (req, res) => {
+  const id = req.params.id;
+  let message = await new Hunter().deleteHunter(id);
+
+  checkObj(message, res);
+});
